e736b879eb9bf035b63ddd4c288f1248f9bad423
gcamp <guillaume.campagna@gmail.com>
2011-07-23 00:51:25 -0400

    Fix a log of bugs. Now working

--- OpenRadarHelper.safariextension/Main.js
+++ OpenRadarHelper.safariextension/Main.js
@@ -101,20 +101,22 @@ function fillDuplicateContent() {
 }
 
 function getMessage(msgEvent) { //The GlobalPage.html returned
-	if (msgEvent.name == "wantsOpenRadar" && msgEvent.message !== "null") saveRadarNumberAndSubmit();
-	else if (msgEvent.name == "wantsDuplicateRadar") {
-		if (msgEvent.message == "yes") fillDuplicateContent();
-		else overwriteSubmitButton();
+	if (msgEvent.name == "openRadar") {
+		if (msgEvent.message[0] == "wantsOpenRadar" && msgEvent.message[1] !== "null") saveRadarNumberAndSubmit();
+		else if (msgEvent.message[0] == "wantsDuplicateRadar") {
+			if (msgEvent.message[1] == "yes") fillDuplicateContent();
+			else overwriteSubmitButton();
+		}	
+		else if (msgEvent.message[1] != "null") document.getElementsByName(msgEvent.message[0])[0].value = msgEvent.message[1];
 	}
-	else if (msgEvent.name == "openRadar") document.getElementsByName(msgEvent.name)[0].value = msgEvent.message;
 	else if (msgEvent.name == "duplicate") {
-		document.getElementById(msgEvent.name).value = msgEvent.message;
+		document.getElementById(msgEvent.message[0]).value = msgEvent.message[1];
 	}
 }
 
+safari.self.addEventListener("message", getMessage, false);
+
 if (document.URL == "http://openradar.appspot.com/myradars/add") fillContent(); //In OpenRadar bug reporter
 else if (document.URL.indexOf("http://openradar.appspot.com/") != -1) addDuplicateButton(); //In OpenRadar, in description page
-else if (document.title.indexOf("New Problem") != -1) safari.self.tab.dispatchMessage("getDatabaseValue", "wantsDuplicateRadar"); //In Apple bug reporter, in "New Problem" page.
-else if (document.title.indexOf("Home") != -1) safari.self.tab.dispatchMessage("getDatabaseValue", "wantsOpenRadar"); //In Apple bug reporter, in the submission confirmation.
-
-safari.self.addEventListener("message", getMessage, false);
+else if (document.title.indexOf("New Problem") != -1) safari.self.tab.dispatchMessage("getOpenRadarValue", "wantsDuplicateRadar"); //In Apple bug reporter, in "New Problem" page.
+else if (document.title.indexOf("Home") != -1) safari.self.tab.dispatchMessage("getOpenRadarValue", "wantsOpenRadar"); //In Apple bug reporter, in the submission confirmation.
